9cac7cb7ad31799f81791f912fefbf4dd44a12e2
Tomas Drencak <tomas@tomas.local>
2017-09-03 12:53:46 +0200

    tests

--- test/statechart_test.clj
+++ test/statechart_test.clj
@@ -7,7 +7,7 @@
             [statechart.transition :as transition]
             [clojure.set :as set]))
 
-(deftest x1
+(deftest test-internal-1
   (let [sc (statechart/make
              {:type        :and
               :states      {:field {:type   :xor
@@ -23,7 +23,7 @@
                (statechart/process-event sc [:focus])
                (get-in [:configuration :configuration]))))))
 
-(deftest x2
+(deftest test-internal-2
   (let [sc (statechart/make
              {:type   :and
               :states {:form {:type        :and
@@ -40,7 +40,7 @@
                (statechart/process-event sc [:focus])
                (get-in [:configuration :configuration]))))))
 
-(deftest x3
+(deftest test-3
   (let [sc (statechart/make
              {:type   :and
               :states {:form {:type        :and
@@ -56,44 +56,3 @@
                (statechart/process-event sc [:focus])
                (get-in [:configuration :configuration]))))))
 
-(let [sc    (statechart/make
-              {:type   :and
-               :states {:form {:type        :and
-                               :states      {:field {:type   :xor
-                                                     :init   :static
-                                                     :states {:static {}
-                                                              :active {}}}}
-                               :transitions [{:event  :focus
-                                              :target [:form :field :active]}
-                                             ]}}})
-      ctx   (-> (statechart/initialize {} sc)
-                (ctx/init-ctx sc [:focus])
-                (ctx/pop-event)
-                #_(statechart/process-event sc [:focus])
-                #_(get-in [:configuration :configuration]))
-      transitions (runtime/event-transitions ctx)]
-  #_[(->> (runtime/transitions-exit-set ctx transitions)
-        (map :id))
-   (->> (runtime/transitions-entry-set ctx transitions)
-        (map :id))]
-
-  (->> #_(runtime/transitions-entry-set ctx transitions)
-    transitions
-    (mapcat (fn [t]
-              (let [ancestor               (runtime/transition-domain t ctx)
-                    immediate-anc-children (->> (runtime/effective-target-states t ctx)
-                                                (map #(last (state/proper-ancestors % ancestor)))
-                                                (remove nil?))
-                    other-children         (if (seq immediate-anc-children)
-                                             (set/difference (into #{} (state/substates ancestor))
-                                                             immediate-anc-children)
-                                             [])
-                    states                 (concat (runtime/add-descendants (transition/target-state t) ctx)
-                                                   (->> (runtime/effective-target-states t ctx)
-                                                        (mapcat #(runtime/add-ancestors % ancestor ctx)))
-                                                   ; this is different than scxml, we need to visit other children
-                                                   ; from transition domain, as they are exited in trasitions-exit-set
-                                                   (->> other-children
-                                                        (mapcat #(runtime/add-descendants % ctx))))]
-                states)))
-       (map :id)))
